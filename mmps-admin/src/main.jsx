f67d535a8647245ee324d8844df5d4c9fb2bba2a
RohitRathi4494 <67454253+RohitRathi4494@users.noreply.github.com>
2026-04-25 03:10:11 +0530

    fix(admin): import Quill CSS and improve skeleton loading UI

--- mmps-admin/src/main.jsx
+++ mmps-admin/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/inter' 
+import 'react-quill/dist/quill.snow.css'
 import './index.css'
 import App from './App.jsx'
 
